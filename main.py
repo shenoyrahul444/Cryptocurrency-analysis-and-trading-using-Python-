523e3de1f966b45cb67310849d0d0c9005284d3f
LAPTOP-JSFNFBTV\Rahul Shenoy <shenoyrahul444@gmail.com>
2018-01-24 19:20:10 -0600

    Checkpoint 1 reached
    Able to get sentiments of the tweets. Need to add more sources to twitter for getting unbiased results.

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class TwitterClient(object):
             print("Error : Authentication Failed")
 
     def clean_tweet(self,tweet):
-            """For cleaning the tweets"""
-                 return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+        """For cleaning the tweets"""
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
     def get_tweet_sentiment(self,tweet):
         """Uses TextBlobs Sendtiment Analysis method to find sentiment of the tweet
@@ -45,24 +45,35 @@ class TwitterClient(object):
         Positive and negative features are extracted from each positive and negative review respectively.
         Training data now consists of labelled positive and negative features. This data is trained on a Naive Bayes Classifier.
         """
+        analysis = TextBlob(self.clean_tweet(tweet))
+#        print(analysis)
+        
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+            
     def get_tweets(self,query,count=10):
+        
         tweets = []
         
         try:
             fetched_tweets = self.api.search(q=query,count = count)
 #            print(fetched_tweets)
-            return fetched_tweets
-#            for tweet in fetched_tweets:
-#                parsed_tweet = {}
-#                parsed_tweet['text'] = tweet.text
-#                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
-#                
-#                if tweet.retweet_count > 0:
-#                    if parsed_tweet not in tweets:
-#                        tweets.append(parsed_tweet)
-#                else:
-#                    tweets.append(parsed_tweet)
-#            return tweets
+#            return fetched_tweets
+            for tweet in fetched_tweets:
+                parsed_tweet = {}
+                parsed_tweet['text'] = tweet.text
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                
+                if tweet.retweet_count > 0:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+            return tweets
         except tweepy.TweepError as e:
             print("Error: %s" % str(e))
             
@@ -72,9 +83,32 @@ def main():
 #    for tweet in public_tweets:
 #        print(tweet.text,"\n")
     tweets = api.get_tweets(query ="litecoin",count = 100)
+    
+    ptweets = []
+    ntweets = []
+    
     for tweet in tweets:
-        print(tweet.text,"\n")
-
+        if tweet['sentiment'] == 'positive':
+            ptweets.append(tweet)
+        elif tweet['sentiment'] == 'negative':
+            ntweets.append(tweet)
+#    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    print('Positive tweets percentage : %s ' % str((len(ptweets)*100)/len(tweets)))
+    
+#    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    print('Negative tweets percentage : %s ' % ((len(ntweets)*100)/len(tweets)))
+    
+    print("***************************** Positive Tweets **********************************\n")
+    for tweet in ptweets:
+        print(tweet['text'],"\n")
+        
+    print('\n\n\n\n')
+    
+    print("***************************** Negative Tweets **********************************\n")
+    for tweet in ntweets:
+        print(tweet['text'],"\n")
+    
+    
 if __name__ == "__main__":
     main()
     
